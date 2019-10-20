8239c8e0330e145b66f9f7f4ec077a1a6f0de494
namnguyen59 <nam.nguyen@groovetechnology.com>
2019-10-20 09:05:20 +0700

    login

---
+++ QuanLyKhachSan/QuanLyKhachSan/Login.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string user = txtUser.Text;
+            string pass = txtPass.Text;
+            if (user == "admin" && pass == "123")
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+            else
+            {
+                DialogResult res = MessageBox.Show("Sai TTT", " ", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information);
+                if (res == DialogResult.Retry)
+                {
+                    txtUser.Focus();
+                }
+                else
+                {
+                    this.DialogResult = DialogResult.Cancel;
+                }
+
+            }
+        }
+    }
+}
